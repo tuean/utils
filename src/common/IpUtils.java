6eb7e598840eea189aa2b5a03199eb002028adf5
zhongxiaotian <tuean_z@163.com>
2018-12-21 17:37:22 +0800

    some new

--- src/common/IpUtils.java
+++ src/common/IpUtils.java
@@ -75,4 +75,36 @@ public class IpUtils {
         }
     }
 
+
+    private String getRealIp(HttpServletRequest request) {
+        String realIp = request.getHeader("x-forwarded-for");
+        if(realIp == null || realIp.length() == 0 || "unknown".equalsIgnoreCase(realIp)) {
+            realIp = request.getHeader("Proxy-Client-IP");
+        }
+        if(realIp == null || realIp.length() == 0 || "unknown".equalsIgnoreCase(realIp)) {
+            realIp = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(realIp == null || realIp.length() == 0 || "unknown".equalsIgnoreCase(realIp)) {
+            realIp = request.getRemoteAddr();
+            if("127.0.0.1".equals(realIp) || "0:0:0:0:0:0:0:1".equals(realIp)){
+                //根据网卡取本机配置的IP
+                InetAddress inet=null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                realIp = inet.getHostAddress();
+            }
+        }
+        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        //"***.***.***.***".length() = 15
+        if(realIp!=null && realIp.length()>15){
+            if(realIp.indexOf(",")>0){
+                realIp = realIp.substring(0,realIp.indexOf(","));
+            }
+        }
+        return realIp;
+    }
+
 }
