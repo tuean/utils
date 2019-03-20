ec69bb68819d4686bb37b45557d2fa2de558a616
zhongxiaotian <tuean_z@163.com>
2019-03-20 11:32:03 +0800

    新增一个获取mapping value的方法

--- src/common/AnnotationUtil.java
+++ src/common/AnnotationUtil.java
@@ -2,7 +2,9 @@ package common;
 
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 
@@ -32,6 +34,21 @@ public class AnnotationUtil {
                 }
                 return base + args[0];
             }
+        }
+        return null;
+    }
+
+
+    /**
+     * 直接通过HttpServletRequest 获取对应的url pattern
+     *
+     * @param request
+     * @return
+     */
+    public static String getRequestMappingUrl(HttpServletRequest request) {
+        Object value = request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE);
+        if (value != null) {
+            return value.toString();
         }
         return null;
     }
